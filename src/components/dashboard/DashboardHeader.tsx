d939455d990e77e9da5bb5a53c4eca56b6779303
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2025-10-02 00:18:34 +0700

    fix: fix responsive dashboard header

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -10,12 +10,12 @@ export function DashboardHeader({
   subtitle = "Kelola Akun dan Konten dengan Mudah" 
 }: DashboardHeaderProps) {
   return (
-    <div className="flex items-center justify-between">
+    <div className="sm:flex items-center justify-between ">
       <div>
         <h1 className="text-3xl font-bold">{title}</h1>
         <p className="text-gray-600 mt-2">{subtitle}</p>
       </div>
-      <div className="text-sm text-gray-600 bg-gray-100 border py-2.5 px-4 rounded-lg flex items-center">
+      <div className="text-sm text-gray-600 bg-gray-100 border py-2.5 px-4 rounded-lg flex items-center mt-4 sm:mt-0">
         <Calendar className="inline mr-2 h-4 w-4" />
         {new Date().toLocaleDateString("id-ID", {
           weekday: "long",
